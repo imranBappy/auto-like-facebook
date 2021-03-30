d6df691a29710088e8c8311f7a482a611fb5188d
imranbappy <imranhosenbappy690@gmail.com>
2021-03-30 23:05:33 +0600

    auth haha

--- app.py
+++ app.py
@@ -3,15 +3,16 @@ import time
 
 pyautogui.FAILSAFE = False
 
-for i in range(0, 10):
-    time.sleep(5)
+for i in range(0,100):
+    time.sleep(1)
     pyautogui.press('j')
-    time.sleep(2)
+    time.sleep(1)
     pyautogui.press('l')
-    time.sleep(2)
-    pyautogui.press('tab')
-    time.sleep(1)
-    pyautogui.press('tab')
-    time.sleep(1)
+    time.sleep(0.50)
+    pyautogui.press('right')   
+    time.sleep(0.30)
+    pyautogui.press('right')   
+    time.sleep(0.30)
+    pyautogui.press('right')   
+    time.sleep(0.30)
     pyautogui.press('enter')
-
